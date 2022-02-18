5d458a898733d0d0bbc0da30f84cea34435ec335
Kartik522001 <panchalkartik30@gmail.com>
2022-02-18 21:50:54 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,32 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const cssStyle = {};
-
-let curDate = new Date();
-curDate = curDate.getHours();
-let greeting = '';
-
-if(curDate >= 1 && curDate < 12 ){
-  greeting = 'Good Morning';
-  cssStyle.color = 'green';
-} 
-else if(curDate >= 12 && curDate < 19){
-  greeting = 'Good Afternoon'
-  cssStyle.color = 'Orange'
-} else{
-  greeting = 'Good Night'
-  cssStyle.color = 'Black'
-}
-
+import App from './App.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-  <div>
-    <h1>Hello Sir, <span style={cssStyle} > {greeting} </span> </h1>
-  </div>
-
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
